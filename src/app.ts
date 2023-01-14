f6c688ba38b440269baf563444d34da99c7317d5
JatinVashisht1 <jatin8085vashisht@gmail.com>
2023-01-14 17:38:10 +0530

    done setup basic error handler middleware and unknown endpoint middleware
    basic error handler middlewares have been setup to handle any error and any wrongly input endpoint. This prevents our app to function wrongly

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 import "dotenv/config";
-import express, { Express } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
+import NoteModel from "./models/note"
 
 const app: Express = express();
 
-app.get("/", (req, res) => {
-    res.status(200).json({ success: true, message: "home page" })
-})
+app.get("/", async (req, res, next) => {
+    /*
+    if our code was synchronous then expree would have automatically called next function
+    if will be changed in next major version of express
+    node version as of writing the comment is 4.18.2
+    default next function calling will be introduced for async code also
+    */
+    try {
+        // throw Error("An unknown error")
+        const notes = await NoteModel.find().exec();
+        return res.status(200).json({ success: true, notes: notes });
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.use((req, res, next)=>{
+   next(Error("Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occurred.";
+    if (error instanceof Error) errorMessage = error.message;
+    return res.status(500).json({ success: false, message: errorMessage });
+});
 
 export default app;
