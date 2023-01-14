67e388eca19679bc6f4e7a597718554940331b89
JatinVashisht1 <jatin8085vashisht@gmail.com>
2023-01-14 20:23:44 +0530

    setup error handling with http-errors package
    Used http-errors package to handle various errors and show specific error codes and messages for various situations. It helps to handle many errors in a clean way.

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,30 @@
 import "dotenv/config";
 import express, { Express, NextFunction, Request, Response } from "express";
+import createHttpError, {isHttpError} from "http-errors";
 import morgan from "morgan"
 import notesRoutes from "../src/routes/notes"
 const app: Express = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 app.use(express.json());
 
 app.use('/api/notes', notesRoutes);
 
 app.use((req, res, next)=>{
-   next(Error("Endpoint not found"));
+   next(createHttpError(404,"Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.error(error);
     let errorMessage = "An unknown error occurred.";
-    if (error instanceof Error) errorMessage = error.message;
-    return res.status(500).json({ success: false, message: errorMessage });
+    let statusCode = 500;
+    if (isHttpError(error)){
+        statusCode = error.status;
+        errorMessage = error.message;
+    } 
+    return res.status(statusCode).json({ success: false, message: errorMessage });
 });
 
 export default app;
