9a74239f240420f7ea9d85295be18ade70aabba9
JatinVashisht1 <jatin8085vashisht@gmail.com>
2023-01-14 17:02:40 +0530

    defined schema for our notes
    Schema for our notes have been created at src/models/note.ts. Type for our schema is also generated by mongoose automatically by using InferSchemaType.

---
+++ src/models/note.ts
@@ -0,0 +1,19 @@
+import { InferSchemaType, model, Schema } from "mongoose";
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+    },
+}, {
+    // will manage timestamps for our database out of the box
+    timestamps: true
+});
+
+// will create a type for our notes inferring from our schema
+type Note = InferSchemaType<typeof noteSchema>
+
+export default model<Note>("Note", noteSchema)
