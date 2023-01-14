07a3c3f7e62eda8835c1c6e013124a571291d326
JatinVashisht1 <jatin8085vashisht@gmail.com>
2023-01-14 16:40:34 +0530

    setup envalid
    used envalid to make sure that our imports from .env are typesafe. App will crash if any parameters are missing in .env file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
-import express, {Express} from "express";
-import * as dotenv from 'dotenv'
-dotenv.config()
-const app: Express = express();
-const PORT = 5000;
+import "dotenv/config";
+import mongoose from "mongoose";
+import express, { Express } from "express";
+import env from "./util/validateEnv"
 
-app.get("/", (req, res)=>{
-    res.status(200).json({success:true, message: "home page"})
+const app: Express = express();
+const PORT = env.PORT
+
+app.get("/", (req, res) => {
+    res.status(200).json({ success: true, message: "home page" })
 })
 
-app.listen(PORT, ()=>{
-    console.log(`server up on http://localhost:${PORT}/`);
-});
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log(`mongoose connected`)
+        app.listen(PORT, () => {
+            console.log(`server up on http://localhost:${PORT}/`);
+        });
+    })
+    // not invoking because we are referencing the function
+    // similar to :: in kotlin
+    .catch(console.error)
